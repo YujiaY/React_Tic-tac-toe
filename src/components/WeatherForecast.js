99b309bd54ad41862eb11a8a2e835ccc3612fd94
Joey <joey.z.rp@gmail.com>
2019-09-20 20:29:05 +1000

    render repeatedly

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -2,33 +2,53 @@ import React from 'react';
 
 import ForecastRow from './ForecastRow';
 
-function WeatherForecast() {
-	return (
-		<section class="weather-forecast">
-			<div class="forecast__switch">
-				<button class="forecast__switch_0 switch-active">5 items</button>
-				<button class="forecast__switch_1">10 items</button>
-			</div>
-			<ForecastRow
-				day="Fri"
-				high="19 c"
-				low="8 c"
-				time="10:00"
-			/>
-			<ForecastRow
-				day="Fri"
-				high="19 c"
-				low="8 c"
-				time="13:00"
-			/>
-			<ForecastRow
-				day="Fri"
-				high="19 c"
-				low="8 c"
-				time="16:00"
-			/>
-		</section>
-	);
+class WeatherForecast extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			forecasts: [
+				{
+					day: 'Mon',
+					high: '10 c',
+					low: '1 c',
+					time: '08:00',
+				},
+				{
+					day: 'Tus',
+					high: '11 c',
+					low: '2 c',
+					time: '09:00',
+				},
+				{
+					day: 'Wed',
+					high: '12 c',
+					low: '3 c',
+					time: '10:00',
+				},
+			],
+		};
+	}
+
+	render() {
+		return (
+			<section class="weather-forecast">
+				<div class="forecast__switch">
+					<button class="forecast__switch_0 switch-active">5 items</button>
+					<button class="forecast__switch_1">10 items</button>
+				</div>
+				{this.state.forecasts.map(forecast => (
+					<ForecastRow
+						key={forecast.day + forecast.time}
+						day={forecast.day}
+						high={forecast.high}
+						low={forecast.low}
+						time={forecast.time}
+					/>
+				))}
+			</section>
+		);
+	}
 }
 
 export default WeatherForecast;

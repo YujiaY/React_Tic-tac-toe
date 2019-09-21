abbba206652cf83990ba0da4470f89697da2a49a
Joey <joey.z.rp@gmail.com>
2019-09-21 13:28:05 +1000

    Lift state up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -7,15 +8,33 @@ import Navigation from './components/Navigation';
 
 import './App.css';
 
-function App() {
-	return (
-		<div className="weather-channel__container">
-			<Header />
-			<Navigation />
-			<Main />
-			<Footer />
-	  	</div>
-	);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			forecasts: [],
+		};
+	}
+
+	componentDidMount() {
+		axios('https://jr-weather-api.herokuapp.com/api/weather?cc=au&city=brisbane')
+			.then(res => {
+				const forecasts = res.data.data.forecast.slice(0, 10);
+				this.setState({ forecasts });
+			});
+	}
+
+	render() {
+		return (
+			<div className="weather-channel__container">
+				<Header />
+				<Navigation />
+				<Main forecasts={this.state.forecasts} />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import axios from 'axios';
 import { format } from 'date-fns';
 
 import ForecastRow from './ForecastRow';
 
 class WeatherForecast extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			forecasts: [],
-		};
-	}
-
-	componentDidMount() {
-		axios('https://jr-weather-api.herokuapp.com/api/weather?cc=au&city=brisbane')
-			.then(res => {
-				const forecasts = res.data.data.forecast.slice(0, 10);
-				this.setState({ forecasts });
-			});
-	}
-
 	render() {
 		return (
 			<section className="weather-forecast">
@@ -28,7 +11,7 @@ class WeatherForecast extends React.Component {
 					<button className="forecast__switch_0 switch-active">5 items</button>
 					<button className="forecast__switch_1">10 items</button>
 				</div>
-				{this.state.forecasts.map(forecast => {
+				{this.props.forecasts.map(forecast => {
 					const date = new Date(forecast.time);
 					const day = format(date, 'EEE');
 					const time = format(date, 'HH:mm');

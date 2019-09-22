e810f083f4397a89d39de289482f283c789e17eb
Joey <joey.z.rp@gmail.com>
2019-09-22 19:41:26 +1000

    Add navigation reducer and actions

---
+++ src/redux/actions/navigationActions.js
@@ -0,0 +1,8 @@
+export const setSearchValue = value => ({
+    value,
+    type: 'SET_SEARCH_VALUE',
+});
+
+export const toggleUnit = () => ({
+    type: 'TOGGLE_UNIT',
+});
---
+++ src/redux/reducers/navigationReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+    search: '',
+    unit: 'c',
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_SEARCH_VALUE':
+            return {
+                ...state,
+                search: action.value,
+            };
+
+        case 'TOGGLE_UNIT':
+            const unit = state.unit === 'c' ? 'f' : 'c';
+
+            return {
+                ...state,
+                unit,
+            };
+
+        default:
+          return state
+      }
+};
